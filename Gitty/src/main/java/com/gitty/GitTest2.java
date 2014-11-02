38dc3e5a12a7d4caf6355eb2064b97c68319d615
Valencia Serrao <divinecreationsvoice@gmail.com>
2014-11-02 08:44:45 +0530

    committing after pushing Pom.xml upstream

--- Gitty/src/main/java/com/gitty/GitTest2.java
+++ Gitty/src/main/java/com/gitty/GitTest2.java
@@ -4,7 +4,7 @@ public class GitTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Praise God!!!! Forever EVER");
+		System.out.println("Praise God!!!! Always");
 	}
 
 }
