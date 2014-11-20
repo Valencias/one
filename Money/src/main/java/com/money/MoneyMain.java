60ab31a5cec697f2f7a486d27cbc16856d260428
Valencia Serrao <divinecreationsvoice@gmail.com>
2014-11-20 08:05:57 +0530

    money committed

--- Money/src/main/java/com/money/MoneyMain.java
+++ Money/src/main/java/com/money/MoneyMain.java
@@ -5,7 +5,7 @@ public class MoneyMain {
 	public static void main(String[] args) {
 		Money m=new Money();
 		m.setAmount(1000);
-		System.out.println("Current amount: Rs. "+m.getAmount());
+		System.out.println("Current amou Rs. "+m.getAmount());
 
 	}
 
