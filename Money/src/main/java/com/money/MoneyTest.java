e6cf06ed043b85ea1e8732a413c3234e888b70c5
Valencia Serrao <divinecreationsvoice@gmail.com>
2014-11-20 08:17:52 +0530

    Money

---
+++ Money/src/main/java/com/money/MoneyTest.java
@@ -0,0 +1,19 @@
+package com.money;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void testSetAmount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAmount() {
+		fail("Not yet implemented");
+	}
+
+}
